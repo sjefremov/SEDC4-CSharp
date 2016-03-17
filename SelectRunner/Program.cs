323fef64722c10a46d6e567ea8bf32375e6a117b
sjefremov <code@SEDC.local>
2016-03-17 18:26:18 +0100

    FirstExerciseDone

--- SelectRunner/Program.cs
+++ SelectRunner/Program.cs
@@ -11,7 +11,11 @@ namespace SelectRunner
         static void Main(string[] args)
         {
             IEnumerable<int> numbers = new List<int> { 2, 3, 5, 7, 8, 11, 13, 16 };
-
+            numbers.Where(n => n % 2 == 1)
+                    .Select(n => Math.Sqrt(n))
+                    .ToList()
+                    .ForEach(Console.WriteLine);
+            Console.ReadLine();
 
         }
     }
