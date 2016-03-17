d4774eb04906f7f83ac157846912b8a34216fe81
sjefremov <code@SEDC.local>
2016-03-17 19:24:55 +0100

    SomeChangesOnProject

--- SelectRunner/Program.cs
+++ SelectRunner/Program.cs
@@ -11,7 +11,7 @@ namespace SelectRunner
         static void Main(string[] args)
         {
             IEnumerable<int> numbers = new List<int> { 2, 3, 5, 7, 8, 11, 13, 16 };
-            numbers.Where(n => n % 2 == 1)
+            numbers.Where(n => n % 2 != 0)
                     .Select(n => Math.Sqrt(n))
                     .ToList()
                     .ForEach(Console.WriteLine);
